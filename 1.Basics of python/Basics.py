8273d7e8087ff6fc92dd26c5608e16ece5baa327
diwakar-web <diwakarrajeshnagar@gmail.com>
2026-01-11 19:49:45 +0530

    first commit

---
+++ 1.Basics of python/Basics.py
@@ -0,0 +1,62 @@
+a=100
+print(a)
+print(type(a))
+
+
+
+
+
+fname= input("Enter Your Name -");
+age=int(input("Enter Your Age -"));
+if(age<18):
+        print(f"Hi {fname} you are {age} years old,thus you are not aligible for driving")
+
+elif(age==18):
+        print(f"Hi {fname} you are {age} years old,thus you can start applying for driving")
+
+else:
+        print(f"Hi {fname} you are {age} years old,thus you are aligible for driving")
+
+
+
+
+
+temp= int(input("Enter the temprature"))
+if(temp<10 and temp >100):
+    print("unbareable")
+elif(temp<20 or temp>90):
+    print("Okay-Okay")
+else:
+    print("You can live")
+
+
+
+
+
+var= input("Enter the letter:")
+if(var in("a","e","i","o","u")):
+    print("Vowels")
+else:
+    print("consonent")
+
+
+
+
+
+i=0
+while(i<5):
+    print(i,end="")
+    i=i+1
+
+for i in range(100,10,-10):
+    print(i, end=".")
+
+for i in range(1, 4):
+        for j in range(1, 4):
+                    print(i, j)
+
+
+
+
+
+
