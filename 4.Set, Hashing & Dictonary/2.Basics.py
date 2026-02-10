83d84cae4421379dbda2123642d5d3062b249c2c
diwakar-web <diwakarrajeshnagar@gmail.com>
2026-02-10 17:20:09 +0530

    commit

---
+++ 4.Set, Hashing & Dictonary/2.Basics.py
@@ -0,0 +1,53 @@
+# set={2,3,2,4,5,100}
+# print(type(set))
+# print(len(set))
+# set.add(100)
+# print(set)
+# set.discard(1008)
+# print(set)
+# set.remove(100)
+# print(set)
+# set.clear()
+# print(set)
+
+# set2=set.copy()
+# set2.add("apple")
+# print(set2)
+
+
+# a={1,2,3,4,5}
+# b={3,4,5,6,7}
+
+# print(a|b)
+# print(a.union(b))
+
+# print(a&b)
+# print(a.intersection(b))
+
+# print(a-b)
+# print(a.difference(b))
+
+# print(a^b)
+# print(a.symmetric_difference(b))
+
+
+
+a={1:'Abhay',2:'Bhanu',3:'Charu',4:'Diwakar',5:'Esha'}
+# print(a)
+# print(a[2])
+# print(a.get(2))
+
+# a[6]="Utpam"
+# print(a)
+
+# a.update({1:"Rakhi",10:"Raju"})
+# print(a)
+
+# a.pop(10)
+# print(a)
+# del a[1]
+# print(a)
+
+print(a.keys())
+print(a.values())
+print(a.items())
